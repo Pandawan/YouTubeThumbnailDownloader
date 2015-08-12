fecad3558ee8aeedf2a0c3d3ebcc531efe7f8793
Pandawan <pandawanfr@gmail.com>
2015-08-12 14:26:52 -0700

    v1.3
    Added Local File Support (experimental, not tested…)

--- YouTubeThumbnailDownloader.sh
+++ YouTubeThumbnailDownloader.sh
@@ -15,9 +15,10 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 
-echo "${green}Hello and welcome to the Youtube Thumbnail Downloader v1.1 by PandawanFr."
+echo "${green}Hello and welcome to the Youtube Thumbnail Downloader v1.3 by PandawanFr."
+echo "${red}Update 1.3: Added File support (experimental)"
 
-read -p "${green}Do you want to download from a video or playlist ? " choice
+read -p "${green}Do you want to download from a video, a playlist or a file ? " choice
 
 if [ "$choice" = "video" ]; then 
 	#If you want to download only one video	
@@ -65,6 +66,33 @@ elif [ "$choice" = "playlist" ]; then
 	
 	echo "${green}Successfully downloaded thumbnails!"
 	echo "${reset}"
+	
+elif [ "$choice" = "file" ]; then
+	#If you want to download a playlist
+	
+	read -p "${green}Enter the path to the file : " link
+	
+	#ids=$(curl --silent $link| grep -F '/watch?v='| sed -E 's!.*/watch\?v=([a-zA-Z0-9_-]*).*!\1!'| sort| uniq;) #Not using this one anymore because V is better
+	
+	ids=(); while read -r; do ids+=("$REPLY"); done < <(curl --silent 'file://$link'| grep -F '/watch?v='| sed -E 's!.*/watch\?v=([a-zA-Z0-9_-]*).*!\1!'| sort| uniq);
+	
+	for ((c=0; c<=${#ids[@]}; c++))
+	 do
+		full_lnk=$prefix${ids[c]}$suffix
+		
+		name=$(curl --silent "https://www.youtube.com/watch?v=${ids[c]}"| grep -o '<meta property="og:title" content=".*">'| sed -e 's/<meta property="og:title" content="\(.*\)">/\1/'| tr " " _)
+		#echo $name > $basedir/hello.txt
+		
+		#echo "Downloading $full_lnk [$c/${#ids[@]}]"
+		echo "Downloading $name.jpg [$c/${#ids[@]}]"
+		
+		#curl -o $basedir/${ids[c]}.jpg --silent $full_lnk
+		
+		curl -o $basedir/$name.jpg --silent $full_lnk
+	 done
+	
+	echo "${green}Successfully downloaded thumbnails!"
+	echo "${reset}"
 
 else
 	echo "${red}"'Please type "video" or "playlist"'
@@ -73,7 +101,7 @@ else
 fi
 
 #Thanks and stuff
-echo "${red}Thanks for using Youtube Thumbnail Downloader v1.1 by PandawanFr!"
+echo "${red}Thanks for using Youtube Thumbnail Downloader v1.3 by PandawanFr!"
 echo "${red}My YouTube: http://youtube.com/PandawanFr/"
 echo "${red}My Twitter: http://twitter.com/PandawanYT/"
 echo " "
